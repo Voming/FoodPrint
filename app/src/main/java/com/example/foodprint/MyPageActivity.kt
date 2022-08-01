240dd34a0e5cad9ceebabd92486cb644e1d907ec
Kim-SeoYoung21 <ksy99921@gmail.com>
2022-08-01 16:37:14 +0900

    Alert

--- app/src/main/java/com/example/foodprint/MyPageActivity.kt
+++ app/src/main/java/com/example/foodprint/MyPageActivity.kt
@@ -6,31 +6,28 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
+import android.view.LayoutInflater
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.item_alert.*
+import java.util.*
 
-class MyPageActivity : AppCompatActivity() {
+class MyPageActivity : AppCompatActivity(), TimePicker.OnTimeChangedListener {
 
     // 연결
     lateinit var editBtn_body: ImageButton
+    lateinit var editBtn_goal: ImageButton
+    lateinit var editBtn_alert: ImageButton
     lateinit var height: TextView
     lateinit var weight: TextView
     lateinit var rate: TextView
-    lateinit var editBtn_goal: ImageButton
-    lateinit var editBtn_alert: ImageButton
+    lateinit var aSwitch: Switch
     lateinit var btn_removeAlert: Button
     lateinit var btn_editalert: Button
-
-    // DB
-    //lateinit var dbManager: AlertDBManager
-    //lateinit var sqlDB: SQLiteDatabase
-
-    // File
-
+    lateinit var alertList: LinearLayout
+    lateinit var alertName: EditText
+    lateinit var alertTime: TimePicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,76 +42,41 @@ class MyPageActivity : AppCompatActivity() {
         rate = findViewById(R.id.rate)
         editBtn_goal = findViewById(R.id.editBtn_goal)
         editBtn_alert = findViewById(R.id.editBtn_alert)
+        //alertList = findViewById(R.id.alertList)
+        //alertName = findViewById(R.id.alertName)
+        alertTime = findViewById(R.id.alertTime)
 
-        //dbManager = AlertDBManager(this, "alertDB", null, 1)
-
-
-
-        // 키, 몸무게, 기초대사량, 목표설정 보이기
-
-        //val a: String = (655.1+(9.56*wei)+(1.85*hei)-(4.68*25)).toString()
-        //rate.setText(a)
 
         // 신체 정보 변경 버튼 클릭 > 편집 dialog 띄우기 > 변경 반영
-        editBtn_body.setOnClickListener{
+        editBtn_body.setOnClickListener {
             var builder = AlertDialog.Builder(this)
             builder.setTitle("신체 정보 변경")
 
             var editView1 = layoutInflater.inflate(R.layout.dialog_body, null)
             builder.setView(editView1)
             builder.show()
-/*
-            editText_weight.findViewById<EditText>(R.id.editText_weight)
-            editText_height.findViewById<EditText>(R.id.editText_height)
-            btn_editBody.findViewById<Button>(R.id.btn_editBody)*/
-
-            // DB 불러오기(키, 몸무게)
-
 
         }
 
 
+        //dialog의 timePicker
+        alertTime = findViewById(R.id.alertTime)
+        alertTime.setOnTimeChangedListener(this)
 
-
-        // 목표 변경 버튼 클릭 > SharedPreferences 사용해 앱 내 폴더에 목표만 저장
-        editBtn_goal.setOnClickListener{
-            /*var builder = AlertDialog.Builder(this)
-            builder.setTitle("목표 변경")
-
-            var editView1 = layoutInflater.inflate(R.layout.dialog_goal, null)
-            builder.setView(editView1)
-            builder.show()*/
-        }
-
-        // 알림 수정: 알림 클릭 시 편집
-
-        // 알림 변경 버튼 클릭: 알림 추가
-        editBtn_alert.setOnClickListener{
+        // 알림 편집 Dialog 띄우기
+        editBtn_alert.setOnClickListener {
 
             var builder2 = AlertDialog.Builder(this)
             builder2.setTitle("신체 정보 변경")
+                .setPositiveButton("확인"){ _, _ ->  }
 
             var editView2 = layoutInflater.inflate(R.layout.dialog_alert, null)
             builder2.setView(editView2)
             builder2.show()
-            //var str_name:String =
-
-
-            //sqlDB = dbManager.writableDatabase
-
-/*
-            btn_removeAlert.setOnClickListener{
-                //sqlDB.execSQL("DELETE FROM alertDB WHERE name = '"+aName+"';")
-            }
-            btn_editalert.setOnClickListener{
-                //sqlDB.execSQL("INSERT INTO alertDB VALUES ('"+aName+"'))
-            }
-*/
         }
 
         // 하단 네비게이션 이동
         bottomNavi.selectedItemId = R.id.action_mypage
-
         bottomNavi.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.action_calendar -> {
@@ -137,15 +99,13 @@ class MyPageActivity : AppCompatActivity() {
             false
         })
     }
-/*
-    // 알람 DB 설정
-    inner class myDBHelper(context: Context) : SQLiteOpenHelper(context, "alert", null, 1) {
-        override fun onCreate(db: SQLiteDatabase?) { // 알람 이름, 시, 분, 실행 여부
-            db!!.execSQL("CREATE TABLE alertDB " +
-                    "(aName text, aHTime Integer, aMTime Integer, aDo bool);")
-        }
-        override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
 
-        }
-    }*/
+    // TimePicker에서 시간이 변경될 때 마다 값을 받아 리스트에 저장
+    override fun onTimeChanged(p0: TimePicker?, p1: Int, p2: Int) {
+        // alertH.text = alertName.toString() //여기서 얻은 내용을 다 리스트에 저장
+
+
+    }
+
+
 }
