df15507f6995e53c5510a7f5dfebec553270f76d
Kim-SeoYoung21 <ksy99921@gmail.com>
2022-08-01 21:34:23 +0900

    MyPage Alert

--- app/src/main/java/com/example/foodprint/MyPageActivity.kt
+++ app/src/main/java/com/example/foodprint/MyPageActivity.kt
@@ -6,12 +6,11 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_my_page.view.*
+import java.sql.Time
 
 class MyPageActivity : AppCompatActivity() {
 
@@ -22,15 +21,8 @@ class MyPageActivity : AppCompatActivity() {
     lateinit var rate: TextView
     lateinit var editBtn_goal: ImageButton
     lateinit var editBtn_alert: ImageButton
-    lateinit var btn_removeAlert: Button
-    lateinit var btn_editalert: Button
 
-    // DB
-    //lateinit var dbManager: AlertDBManager
-    //lateinit var sqlDB: SQLiteDatabase
-
-    // File
-
+    lateinit var layout: LinearLayout //알림 리스트가 보여질 레이아웃
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,13 +38,6 @@ class MyPageActivity : AppCompatActivity() {
         editBtn_goal = findViewById(R.id.editBtn_goal)
         editBtn_alert = findViewById(R.id.editBtn_alert)
 
-        //dbManager = AlertDBManager(this, "alertDB", null, 1)
-
-
-
-        // 키, 몸무게, 기초대사량, 목표설정 보이기
-
-
         //val a: String = (655.1+(9.56*wei)+(1.85*hei)-(4.68*25)).toString()
         //rate.setText(a)
 
@@ -64,25 +49,58 @@ class MyPageActivity : AppCompatActivity() {
             var editView1 = layoutInflater.inflate(R.layout.dialog_body, null)
             builder.setView(editView1)
             builder.show()
+
+            //builder.setPositiveButton("적용하기", new OnClickListener(){
+
+            //})
 /*
             editText_weight.findViewById<EditText>(R.id.editText_weight)
             editText_height.findViewById<EditText>(R.id.editText_height)
             btn_editBody.findViewById<Button>(R.id.btn_editBody)*/
-
-            // DB 불러오기(키, 몸무게)
-
-
         }
-
-
-
 
         // 목표 변경 버튼 클릭 > SharedPreferences 사용해 앱 내 폴더에 목표만 저장
         editBtn_goal.setOnClickListener{
             showAddGoal()
         }
 
-        // 알림 수정: 알림 클릭 시 편집
+        // 알림
+        layout = findViewById(R.id.alertList)
+        var alertList = ArrayList<Alr>()
+        alertList.add(Alr("아침",6,20))
+        alertList.add(Alr("점심",12,30))
+        alertList.add(Alr("저녁",18,15))
+
+
+        //update_alert(alertList)
+        var num: Int =0
+        for(i in alertList)
+        {
+            var str_name = i.name
+            var str_hour = i.hour
+            var str_min = i.min
+
+            var layout_items: LinearLayout = LinearLayout(this)
+            layout_items.orientation = LinearLayout.HORIZONTAL
+            layout_items.id = num
+            layout_items.setTag(str_name)
+
+            layout.setPadding(40,20,40,10)
+
+            var tvName: TextView = TextView(this)
+            tvName.text = str_name
+            tvName.textSize = 18f
+            layout_items.addView(tvName)
+
+            var tvHour: TextView = TextView(this)
+            tvHour.text = "                                        "+str_hour.toString()+" : "+str_min.toString()
+            tvHour.textSize = 18f
+            layout_items.addView(tvHour)
+
+            layout.addView(layout_items)
+            num++
+
+        }
 
         // 알림 변경 버튼 클릭: 알림 추가
         editBtn_alert.setOnClickListener{
@@ -90,22 +108,27 @@ class MyPageActivity : AppCompatActivity() {
             var builder2 = AlertDialog.Builder(this)
             builder2.setTitle("신체 정보 변경")
 
+
+            val dialogAlertView = layoutInflater.inflate(R.layout.dialog_alert,null)
+            val a_name : EditText by lazy {
+                dialogAlertView.findViewById(R.id.alertName)
+            }
+            val a_hour : TimePicker by lazy {
+                dialogAlertView.findViewById(R.id.alertTime)
+            }
+
             var editView2 = layoutInflater.inflate(R.layout.dialog_alert, null)
             builder2.setView(editView2)
+                .setPositiveButton("적용하기"){
+                        dialogInterface, i ->
+                    var n = a_name.getText().toString()
+                    var h = a_hour.hour
+                    var m = a_hour.minute
+                    //alertList.add(Alr(a_name.text.toString(),h, m))
+                    update_alert(Alr(n, h, m),num++)
+                }
             builder2.show()
-            //var str_name:String =
 
-
-            //sqlDB = dbManager.writableDatabase
-
-/*
-            btn_removeAlert.setOnClickListener{
-                //sqlDB.execSQL("DELETE FROM alertDB WHERE name = '"+aName+"';")
-            }
-            btn_editalert.setOnClickListener{
-                //sqlDB.execSQL("INSERT INTO alertDB VALUES ('"+aName+"'))
-            }
-*/
         }
 //
         // 하단 네비게이션 이동
@@ -133,17 +156,6 @@ class MyPageActivity : AppCompatActivity() {
             false
         })
     }
-/*
-    // 알람 DB 설정
-    inner class myDBHelper(context: Context) : SQLiteOpenHelper(context, "alert", null, 1) {
-        override fun onCreate(db: SQLiteDatabase?) { // 알람 이름, 시, 분, 실행 여부
-            db!!.execSQL("CREATE TABLE alertDB " +
-                    "(aName text, aHTime Integer, aMTime Integer, aDo bool);")
-        }
-        override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
-
-        }
-    }*/
 
     //목표 변경 다이얼로그
     fun showAddGoal(){
@@ -155,12 +167,42 @@ class MyPageActivity : AppCompatActivity() {
             findViewById(R.id.goal)
         }
 
-        var builder = AlertDialog.Builder(this)
+        /*var builder = AlertDialog.Builder(this)
         val dialog = builder.setView(goalDialogView)
             .setPositiveButton("적용하기"){
                     dialogInterface, i ->
                 goal.setText(edt_goal.text) }
             .create()
-        dialog.show()
+        dialog.show()*/
     }
+
+    fun update_alert(al: Alr, num: Int){
+        var str_name = al.name
+        var str_hour = al.hour
+        var str_min = al.min
+        var numb :Int = num
+
+        var layout_items: LinearLayout = LinearLayout(this)
+        layout_items.orientation = LinearLayout.HORIZONTAL
+        layout_items.id = numb
+        layout_items.setTag(str_name)
+
+        layout.setPadding(40,20,40,10)
+
+        var tvName: TextView = TextView(this)
+        tvName.text = str_name
+        tvName.textSize = 18f
+        layout_items.addView(tvName)
+
+        var tvHour: TextView = TextView(this)
+        tvHour.text = "                                        "+str_hour.toString()+" : "+str_min.toString()
+        tvHour.textSize = 18f
+        layout_items.addView(tvHour)
+
+        layout.addView(layout_items)
+    }
+
 }
+
+// 알림 출력을 위한 클래스
+class Alr(val name: String, val hour: Int, val min: Int) { }
